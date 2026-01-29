f5ca1e080e60c42b3fb8214d0c2adbef5fd0461c
Adil ABBADI <adil.abbadi.1996@gmail.com>
2026-01-29 21:33:15 +0000

    Update layout to use PNG favicon, remove SVG favicon, and adjust next-env.d.ts for development route types.

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
         {/* Google Tag Manager */}
         {gtmId && (
           <Script

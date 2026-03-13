69cda8ccc92952ed7760d411f970dff1812f7cb4
Adil ABBADI <adil.abbadi.1996@gmail.com>
2026-03-13 01:24:45 +0000

    Add Meta Pixel integration to layout and update environment example

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const gtmId = process.env.NEXT_PUBLIC_GTM_ID || "";
+  const metaPixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID || "";
 
   return (
     <html lang="en" suppressHydrationWarning>
@@ -46,6 +47,25 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             }}
           />
         )}
+        {/* Meta Pixel */}
+        {metaPixelId && (
+          <Script
+            id="meta-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${metaPixelId}');
+fbq('track', 'PageView');`,
+            }}
+          />
+        )}
       </head>
       <body
         className={`${interTight.variable} font-sans bg-white dark:bg-black min-h-screen`}
@@ -58,6 +78,19 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               height="0"
               width="0"
               style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        )}
+        {/* Meta Pixel (noscript) */}
+        {metaPixelId && (
+          <noscript>
+            {/* eslint-disable-next-line @next/next/no-img-element -- 1x1 tracking pixel, next/image not suitable */}
+            <img
+              height={1}
+              width={1}
+              style={{ display: "none" }}
+              src={`https://www.facebook.com/tr?id=${metaPixelId}&ev=PageView&noscript=1`}
+              alt=""
             />
           </noscript>
         )}
